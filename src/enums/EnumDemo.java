97b5930cc8cc38a9bc8b154526baf96f706338b2
Nandivaleamol <nandivaleamol@gmail.com>
2022-11-07 18:13:04 +0530

    Initial commit

---
+++ src/enums/EnumDemo.java
@@ -0,0 +1,36 @@
+package enums;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+enum Beer{
+    KF, KO, RC, FO;
+}
+
+public class EnumDemo {
+    public static void main(String[] args) {
+
+        Beer beer1 = Beer.KF;
+        System.out.println(beer1);
+
+        Beer[] beer = Beer.values();
+
+        for(Beer beer2 : beer){
+            System.out.println(beer2 +"----"+beer2.ordinal());
+
+        }
+
+
+
+
+
+       /* // stream api
+        System.out.println("Iterating using stream api");
+        Arrays.stream(beer).forEach(System.out::println);
+
+        System.out.println();
+        List<Beer> b = List.of(beer);
+        b.forEach(System.out::println);*/
+    }
+}
